2998654c06fd63a3f5a2fe2caa1b50ca40598a17
Hank Andre <hank.andre@irishtitan.com>
2016-12-11 23:55:41 -0600

    added webpack

--- src/app/app.js
+++ src/app/app.js
@@ -6,10 +6,9 @@ import uirouter from 'angular-ui-router';
 import appComponent from './app.component';
 import StaticElementsModule from './static-elements/static-elements';
 
-(function() {
-    angular.module('app', [
-        uirouter,
-        StaticElementsModule.name
-    ])
-    .component('app', appComponent);
-})();
+
+angular.module('app', [
+    uirouter,
+    StaticElementsModule.name
+])
+.component('app', appComponent);
--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
         loaders: [
             { test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'ng-annotate!babel' },
             { test: /\.html$/, loader: 'raw' },
-            { test: /\.scss$/, loader: 'style!css!sass!autoprefixer?{browsers: ">5%"}' },
+            { test: /\.scss$/, loader: 'style!css!postcss!sass' },
             { test: /\.css$/, loader: 'style!css' }
             // { test: /\.(ttf|otf|eot|svg|woff(2)?)$/, loader: 'url' }
         ]
